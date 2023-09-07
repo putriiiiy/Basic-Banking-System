d8616a8ce82275cad1157104fb4190b20f7da81c
putriiiiy <141803577+putriiiiy@users.noreply.github.com>
2023-09-08 00:01:11 +0700

    Update banking_system.js

--- banking_system.js
+++ banking_system.js
@@ -1,35 +1,71 @@
-class BankAccount{
-  constructor(){
-    this.saldo = Number();
+//class BankAccount
+class BankAccount {
+  //constractor
+  constructor(saldo) {
+      this.saldo = saldo;
   }
 
-deposit(amount){
-  setTimeout(() => {
-    this.saldo += amount;
-    //alert(`SALDO terbaru Anda saat ini sebesar Rp. ${this.saldo}`);
-    console.log(`SALDO terbaru Anda saat ini sebesar Rp. ${this.saldo}`);
-    }, 1000);
+  //method deposit
+  deposit(amount) {
+      return new Promise((resolve) => {
+          setTimeout(() => {
+              this.saldo += amount;
+              console.log(`SALDO terbaru Anda saat ini sebesar Rp. ${this.saldo}`);
+              alert(`SALDO terbaru Anda saat ini sebesar Rp. ${this.saldo}`);
+              resolve();
+          }, 1000);
+      });
+  }
+
+  //method withdraw
+  withdraw(amount) {
+      return new Promise((resolve) => {
+          setTimeout(() => {
+              if (amount <= this.saldo) {
+                  this.saldo -= amount;
+                  console.log(`SALDO terbaru Anda saat ini sebesar Rp. ${this.saldo}`);
+                  alert(`SALDO terbaru Anda saat ini sebesar Rp. ${this.saldo}`);
+              } else {
+                  console.log(`Saldo Anda Tidak Cukup! Saldo Anda hanya sebesar Rp.${this.saldo}`);
+                  alert(`Saldo Anda Tidak Cukup! Saldo Anda hanya sebesar Rp.${this.saldo}`);
+              }
+              resolve();
+          }, 1000);
+      });
+  }
 }
 
-withdraw(amount){
-  setTimeout(() => {
-    if(amount <= this.saldo){
-      this.saldo -= amount;
-      //alert(`SALDO terbaru Anda saat ini sebesar Rp. ${this.saldo}`);
-      console.log(`SALDO terbaru Anda saat ini sebesar Rp. ${saldo}`);
-    }else {
-      console.log (`Saldo Anda Tidak Cukup! Saldo Anda hanya sebesar Rp. ${this.saldo}`);
-    }
-  }, 2000};
-}
-}
 
-//pemanggilan class
-let putri = new BankAccount();
-putri.deposit(100000); // menambahkan saldo
-putri.withdraw(20000); // mengambil saldo pada rekening
-putri. withdrw (10000); // mengambil saldo, namun saldo tidak menucukupi
+let inputSaldo; //deklarasi variabel inputSaldo
+const account = new BankAccount(0); // membuat objek baru dengan Saldo awal default adalah 0
+
+//fungsi main menggunakan async await
+async function main() {
+      do {
+          choice = window.prompt(`
+              Pilih kondisi yang Anda inginkan!
+              1. Menambah saldo
+              2. Mengurangi saldo`);
+
+          switch (choice) {
+              case '1':
+                //tambah saldo
+                  inputSaldo = Number(window.prompt('Masukkan jumlah saldo yang ingin ditambah : '));
+                  await account.deposit(inputSaldo);
+                  break;
+              case '2':
+                //kurangi saldo
+                  inputSaldo = Number(window.prompt('Masukkan jumlah saldo yang ingin ditarik : '));
+                  await account.withdraw(inputSaldo);
+                  break;
+              default:
+                  console.log('Pilihan tidak valid. Silakan coba lagi.');
+                  break;
+          }
+
+          pilihan = confirm('Apakah ingin menambah/menarik saldo kembali?');
+      } while (pilihan);
+  };
 
 
-
-      
+main();
